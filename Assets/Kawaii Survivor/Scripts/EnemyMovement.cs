363fdfda6c04c3d13e277f981e3fc9a0ed94c8d3
MostafaEbeed <en.mostfa4@gmail.com>
2024-10-16 11:38:32 +0300

    EnemySpawnSequence

--- Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
+++ Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
@@ -5,8 +6,20 @@ public class EnemyMovement : MonoBehaviour
     [Header("Elements")]
     private Player player;
 
+    [Header("Spawn Sequence Related")]
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private SpriteRenderer spawnIndicator;
+    private bool hasSpawned;
+
     [Header("Settings")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float playerDetectionRadius;
+
+    [Header("Header")]
+    [SerializeField] private ParticleSystem passAwayParticles;
+
+    [Header("Debug")]
+    [SerializeField] private bool displayGizmos;
 
     void Start()
     {
@@ -17,14 +30,66 @@ public class EnemyMovement : MonoBehaviour
             Debug.LogWarning("No Player Found");
             Destroy(gameObject);
         }
+
+        spriteRenderer.enabled = false;
+
+        spawnIndicator.enabled = true;
+
+        Vector3 targetScale = spawnIndicator.transform.localScale * 1.2f;
+        LeanTween.scale(spawnIndicator.gameObject, targetScale, 0.3f)
+            .setLoopPingPong(4)
+            .setOnComplete(SpawnSequnceCompleted);
     }
 
     void Update()
     {
+        if(!hasSpawned) return;
+
+        FollowPlayer();
+
+        TryAttack();
+    }
+
+    private void SpawnSequnceCompleted()
+    {
+        spriteRenderer.enabled = true;
+
+        spawnIndicator.enabled = false;
+
+        hasSpawned = true;
+    }
+
+    private void FollowPlayer()
+    {
         Vector2 direction = (player.transform.position - transform.position).normalized;
-        
+
         Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
 
         transform.position = targetPosition;
+    }
+
+    private void TryAttack()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if(distanceToPlayer <= playerDetectionRadius)
+            PassAway();
+    }
+
+    private void PassAway()
+    {
+        passAwayParticles.transform.SetParent(null);
+        passAwayParticles.Play();
+
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!displayGizmos) return;
+
+        Gizmos.color = Color.red;
+        
+        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
     }
 }
